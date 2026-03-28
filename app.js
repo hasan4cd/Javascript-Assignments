409e5ffe96ed1f06d59be8f852d1085e39455b24
hasan4cd <hasan4cd@gmail.com>
2026-03-28 15:55:13 +0500

    Work Accomplish

---
+++ app.js
@@ -0,0 +1,296 @@
+// Chapter 1
+// ans 1
+alert("Hi! This is my Javascript Exercises");
+
+// ans 2
+alert("Error! Please enter a valid password.");
+
+// ans 3
+alert("Welcome to JS Land...\nHappy Coding!");
+
+// ans 4
+alert("Welcome to JS Land...");
+alert("Happy Coding!\nPrevent this page from creating additional dialogs.");
+
+// ans 5
+console.log("Hello... I can run JS through my web browser's console");
+
+// ans 6
+alert("How are you?");
+
+// Chapter 2
+// ans 1
+var username;
+
+// ans 2
+var myName = "Muhammad Hasan Raza";
+
+// ans 3
+var message = "Hello World";
+alert(message);
+
+// ans 4
+var name = "Jhone Doe";
+var age = 15;
+var course = "Certified Mobile Application Development";
+alert(name);
+alert(age + " years old");
+alert(course);
+
+// ans 5
+alert("PIZZA\nPIZZ\nPIZ\nPI\nP");
+
+// ans 6
+var email = "example@example.com";
+alert("My email address is " + email);
+
+// ans 7
+var book = "A smarter way to learn JavaScript";
+alert("I am trying to learn from the Book " + book);
+
+// ans 8
+document.writeln("Yah! I can write HTML content through JavaScript");
+
+// ans 9
+var empty = "";
+alert(empty);
+document.writeln(empty);
+
+// Chapter 3
+// ans 1
+var age = 15;
+alert("I am " + age + " years old");
+
+// ans 2
+var visitCount = 14;
+alert("You have visited this site " + visitCount + " times");
+
+// ans 3
+var birthYear = 1990;
+document.writeln("My birth year is " + birthYear);
+document.writeln("Data type of my declared variable is number");
+
+// ans 4
+var visitorName = "John Doe";
+var productTitle = "T-shirt";
+var quantity = 5;
+document.writeln(visitorName + " ordered " + quantity + " " + productTitle + "(s) on XYZ Clothing store");
+
+// Chapter 4
+// ans 1
+var name, age, course;
+
+// ans 2
+// 5 Legal variable names
+var myName;
+var age;
+var courseTitle;
+var $price;
+var _username;
+// 5 Illegal variable names
+// var 1name;
+// var my-name;
+// var my name;
+// var @course;
+// var #price;
+
+// ans 3
+document.writeln("<h1>Rules for naming JS variables</h1><br>");
+document.writeln("Variable names can only contain letters, numbers, $ and _. For example: $my_1stVariable<br>");
+document.writeln("Variables must begin with a letter, $ or _. For example: $name, _name or name<br>");
+document.writeln("Variable names are case sensitive<br>");
+document.writeln("Variable names should not be JS keywords<br><br>");
+
+// Chapter 5
+// ans 1
+var num1 = 3;
+var num2 = 5;
+var sum = num1 + num2;
+document.writeln("The sum of " + num1 + " and " + num2 + " is " + sum + "<br>");
+
+// ans 2
+var num1 = 10;
+var num2 = 5;
+var difference = num1 - num2;
+var product = num1 * num2;
+var quotient = num1 / num2;
+var modulus = num1 % num2;
+document.writeln("The difference of " + num1 + " and " + num2 + " is " + difference + "<br>");
+document.writeln("The product of " + num1 + " and " + num2 + " is " + product + "<br>");
+document.writeln("The quotient of " + num1 + " and " + num2 + " is " + quotient + "<br>");
+document.writeln("The modulus of " + num1 + " and " + num2 + " is " + modulus + "<br><br>");
+
+// ans 3
+var num;
+document.writeln("Value after variable declaration is: " + num + "<br>");
+num = 5;
+document.writeln("Initial value: " + num + "<br>");
+num++;
+document.writeln("Value after increment is: " + num + "<br>");
+num += 7;
+document.writeln("Value after addition is: " + num + "<br>");
+num--;
+document.writeln("Value after decrement is: " + num + "<br>");
+var remainder = num % 3;
+document.writeln("The remainder is: " + remainder + "<br><br>");
+
+// ans 4
+var ticketPrice = 600;
+var totalCost = ticketPrice * 5;
+document.writeln("Total cost to buy 5 tickets to a movie is " + totalCost + " PKR<br><br>");
+
+// ans 5
+var num = 4;
+document.writeln("Table of " + num + "<br>");
+for (var i = 1; i <= 10; i++) {
+    document.writeln(num + " x " + i + " = " + (num * i) + "<br>");
+}
+
+// ans 6
+var celsius = 25;
+var fahrenheit = (celsius * 9/5) + 32;
+document.writeln("<br>" + celsius + "°C is " + fahrenheit + "°F<br>");
+var fahrenheit2 = 70;
+var celsius2 = (fahrenheit2 - 32) * 5/9;
+document.writeln(fahrenheit2 + "°F is " + celsius2 + "°C<br><br>");
+
+// ans 7
+var item1Price = 650;
+var item2Price = 100;
+var item1Quantity = 3;
+var item2Quantity = 7;
+var shippingCharges = 100;
+var totalCost = (item1Price * item1Quantity) + (item2Price * item2Quantity) + shippingCharges;
+document.writeln("Price of item 1 is " + item1Price + "<br>");
+document.writeln("Quantity of item 1 is " + item1Quantity + "<br>");
+document.writeln("Price of item 2 is " + item2Price + "<br>");
+document.writeln("Quantity of item 2 is " + item2Quantity + "<br>");
+document.writeln("Shipping Charges " + shippingCharges + "<br><br>");
+document.writeln("Total cost of your order is " + totalCost + "<br><br>");
+
+// ans 8
+var totalMarks = 980;
+var marksObtained = 804;
+var percentage = (marksObtained / totalMarks) * 100;
+document.writeln("Total marks: " + totalMarks + "<br>");
+document.writeln("Marks obtained: " + marksObtained + "<br>");
+document.writeln("Percentage: " + percentage + "%<br><br>");
+
+// ans 9
+var usDollars = 10;
+var saudiRiyals = 25;
+var totalPKR = (usDollars * 104.80) + (saudiRiyals * 28);
+document.writeln("Total currency in PKR: " + totalPKR + "<br>");
+
+// ans 10
+var num = 10;
+var result = ((num + 5) * 10) / 2;
+document.writeln("The result of the expression is: " + result + "<br><br>");
+
+// ans 11
+var currentYear = 2026;
+var birthYear = 2005;
+var age = currentYear - birthYear;
+document.writeln("Your age is: " + age + "<br><br>");
+
+// ans 12
+var radius = 20;
+var circumference = 2 * Math.PI * radius;
+var area = Math.PI * Math.pow(radius, 2);
+document.writeln("The circumference is: " + circumference.toFixed(2) + "<br>");
+document.writeln("The area is: " + area.toFixed(2) + "<br><br>");
+
+// ans 13
+var favoriteSnack = "Chocolate Chip";
+var currentAge = 15;
+var maxAge = 65;
+var amountPerDay = 3;
+var totalSnacks = (maxAge - currentAge) * amountPerDay * 365;
+document.writeln("You will need " + totalSnacks + " " + favoriteSnack + " to last you until the ripe old age of " + maxAge + "<br><br>");
+
+// Chapter 12-13
+// ans 1
+var char = prompt("Enter a character:");
+if (char.length === 1) {
+    var asciiCode = char.charCodeAt(0);
+    if (asciiCode >= 48 && asciiCode <= 57) {
+        document.writeln(char + " is a number.<br><br>");
+    } else if (asciiCode >= 65 && asciiCode <= 90) {
+        document.writeln(char + " is an uppercase letter.<br><br>");
+    } else if (asciiCode >= 97 && asciiCode <= 122) {
+        document.writeln(char + " is a lowercase letter.<br><br>");
+    } else {
+        document.writeln(char + " is a special character.<br><br>");
+    }
+} else {
+    document.writeln("Please enter a single character.<br><br>");
+}
+
+// ans 2
+var num1 = 8;
+var num2 = 12;
+if (num1 > num2) {
+    document.writeln(num1 + " is larger than " + num2 + ".<br><br>");
+} else if (num2 > num1) {
+    document.writeln(num2 + " is larger than " + num1 + ".<br><br>");
+} else {
+    document.writeln("Both numbers are equal.<br><br>");
+}
+
+// ans 3
+var num = +prompt("Enter a number:");
+if (num > 0) {
+    document.writeln(num + " is a positive number.<br><br>");
+} else if (num < 0) {
+    document.writeln(num + " is a negative number.<br><br>");
+} else {
+    document.writeln(num + " is zero.<br><br>");
+}    
+
+// ans 4
+var char = prompt("Enter a character:");
+if (char.length === 1) {
+    var lowerChar = char.toLowerCase();
+    if (lowerChar === 'a' || lowerChar === 'e' || lowerChar === 'i' || lowerChar === 'o' || lowerChar === 'u') {
+        document.writeln(char + " is a vowel.<br><br>");
+    } else {
+        document.writeln(char + " is not a vowel.<br><br>");
+    }
+} else {
+    document.writeln("Please enter a single character.<br><br>");
+}
+
+// ans 5
+var correctPassword = "abc123";
+var userPassword = prompt("Enter your password:");
+if (userPassword === "") {
+    document.writeln("Please enter your password.<br><br>");
+} else if (userPassword === correctPassword) {
+    document.writeln("Correct! The password you entered matches the original password.<br><br>");
+} else {
+    document.writeln("Incorrect password.<br><br>");
+}
+
+// ans 6
+var greeting;
+var hour = 13;
+if (hour < 18) {
+    greeting = "Good day";
+} else {
+    greeting = "Good evening";
+}
+document.writeln(greeting);
+
+// ans 7
+var time = +prompt("Enter time in 24-hour format (e.g., 1900 for 7 PM):");
+if (time >= 0 && time < 1200) {
+    document.writeln("Good morning!");
+} else if (time >= 1200 && time < 1700) {
+    document.writeln("Good afternoon!");
+} else if (time >= 1700 && time < 2100) {
+    document.writeln("Good evening!");
+}else if (time >= 2100 && time < 2359) {
+    document.writeln("Good night!");
+} else {
+    document.writeln("Invalid time format. Please enter a valid time in 24-hour format.");
+}
